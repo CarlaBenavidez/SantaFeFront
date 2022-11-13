d7ee9b5b10d28adb146cbe8e29a2781db9c5e217
Carla <109388578+CarlaBenavidez@users.noreply.github.com>
2022-11-13 16:10:37 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -9,9 +9,10 @@ function inclusive()
     {
         document.body.style.backgroundColor= "black";
         document.body.style.color= "white";
-
-
-
+document.getElementById("aside").style.backgroundColor="black"
+document.getElementById("foot").style.backgroundColor="black"
+document.getElementById("head").style.backgroundColor="black"
+document.getElementById("nav").style.backgroundColor="white"
 
 
 
