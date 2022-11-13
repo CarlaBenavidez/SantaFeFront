d812abd6d13ac5c8bdfbfe5cb7b6171ff8217123
Carla <109388578+CarlaBenavidez@users.noreply.github.com>
2022-11-13 20:12:49 -0300

    Add files via upload

---
+++ js/javascript2.js
@@ -0,0 +1,31 @@
+
+
+function DatosDeLaCreadora() {
+    alert("Carla Benavidez.")
+}
+
+
+function inclusive()
+    {
+        document.body.style.backgroundColor= "black";
+        document.body.style.color= "white";
+document.getElementById("aside").style.backgroundColor="black"
+document.getElementById("foot").style.backgroundColor="black"
+document.getElementById("head").style.backgroundColor="black"
+document.getElementById("nav").style.backgroundColor="white"
+
+
+    }
+
+
+    function inclusiva()
+        {
+            document.body.style.backgroundColor= "none";
+            document.body.style.color= "white";
+    document.getElementById("aside").style.backgroundColor="black"
+    document.getElementById("foot").style.backgroundColor="black"
+    document.getElementById("head").style.backgroundColor="black"
+    document.getElementById("nav").style.backgroundColor="white"
+
+
+        }
