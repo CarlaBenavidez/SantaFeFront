8baa90755c47073188a2829d07a1edb3fe95560e
Carla <109388578+CarlaBenavidez@users.noreply.github.com>
2022-11-13 21:12:54 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -15,5 +15,18 @@ document.getElementById("head").style.backgroundColor="black"
 document.getElementById("nav").style.backgroundColor="white"
 
 
-
     }
+
+
+    function inclusiva()
+        {
+            document.body.style.backgroundColor= "none";
+            document.body.style.color= "white";
+    document.getElementById("aside").style.backgroundColor="black"
+    document.getElementById("foot").style.backgroundColor="black"
+    document.getElementById("head").style.backgroundColor="black"
+    document.getElementById("nav").style.backgroundColor="white"
+    document.getElementByClassName("card").style.backgroundColor="Black"
+
+
+        }
